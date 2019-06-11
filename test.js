770478937f85861058395793317749ddbd062e9e
Виктория <Ovcharka@gmail.com>
2019-06-11 19:13:27 +0300

    бинарный поиск с тестами

--- test.js
+++ test.js
@@ -2,22 +2,21 @@ describe("BinarySearch", function() {
 
   describe("Поиск элемента в массиве", function() {
 
+    var arr = [1, 2, 5, 6, 8, 10, 12, 90];
+
     function makeTest(message, x, result) {
-      var arr = [2, 1, 5, 6, 8, 10, 90, 12];
-      it(message + x  + ' результат: ' + result, function() {
+      it(message + x  + ' индекс равен: ' + result, function() {
         assert.equal(BinarySearch(x, arr), result);
       });
     }
 
-    makeTest('при поиске чиcла ', 2, 2);
-    makeTest('при поиске чиcла ', 12, 12);
-    makeTest('при поиске чиcла ', 1, 1);
-    makeTest('при поиске чиcла ', 90, 90);
-    makeTest('при поиске чиcла ', 10, 10);
+    makeTest('при поиске первого числа в массиве ', arr[0], 0);
+    makeTest('при поиске среднего чиcла в массиве ', 6, 3);
+    makeTest('при поиске последнего числа в массиве ', arr[arr.length-1], arr.length-1);
     makeTest('при поиске несуществующего чиcла ', 100, -1);
-    makeTest('при поиске чиcла в виде строки ', '1', 1);
-    makeTest('при поиске чиcла в виде строки ', '2', 2);
-    makeTest('при поиске чиcла в виде строки ', '12', 12);
+    makeTest('при поиске чиcла в виде строки ', '1', 0);
+    makeTest('при поиске чиcла в виде строки ', '2', 1);
+    makeTest('при поиске чиcла в виде строки ', '12', 6);
     makeTest('при поиске неккоректного чиcла ', '2px', -1);
   });
 });
